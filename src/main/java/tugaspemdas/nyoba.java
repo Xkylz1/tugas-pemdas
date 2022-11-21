ae7c63d41de75139d27b5de8e3c256d59ba4c790
Xkylz1 <fauzansaputra741@gmail.com>
2022-11-21 20:45:44 +0800

    jajar genjang masih error cuy

--- src/main/java/tugaspemdas/nyoba.java
+++ src/main/java/tugaspemdas/nyoba.java
@@ -5,50 +5,101 @@ import java.util.Scanner;
 
 public class nyoba {
     public static void main(String[] args) {
-Scanner input = new Scanner(System.in);
-        System.out.print("Masukan panjang : ");
-        int panjangSisi = input.nextInt();
-        System.out.print("Masukan lebar : ");
-        int lebar = input.nextInt();
-        persegiPanjang(panjangSisi,lebar);
+//        Scanner input = new Scanner(System.in);
+//        System.out.print("Masukan panjang : ");
+//        int panjangSisi = input.nextInt();
+//        System.out.print("Masukan tinggi : ");
+//        int lebar = input.nextInt();
+//        jajarGenjang(panjangSisi, lebar);
 //        int panjangSisi=5;
 //        persegi(panjangSisi);
+        jajar();
 
     }
 
-    public static void persegi(int sisi){
-        for(int i =0;i<sisi;i++) {
+    public static void persegi(int sisi) {
+        for (int i = 0; i < sisi; i++) {
             System.out.print("*  ");
         }
-        for (int i=2;i<sisi;i++){
+        for (int i = 2; i < sisi; i++) {
             System.out.println();
             System.out.print("*  ");
-            for(int j=2;j<sisi;j++){
+            for (int j = 2; j < sisi; j++) {
                 System.out.print("   ");
             }
             System.out.print("*  ");
         }
         System.out.println();
-        for(int i =0;i<sisi;i++) {
+        for (int i = 0; i < sisi; i++) {
             System.out.print("*  ");
         }
     }
 
-    public static void persegiPanjang(int panjang, int lebar){
-        for (int i = 0;i<panjang;i++){
+    public static void persegiPanjang(int panjang, int lebar) {
+        for (int i = 0; i < panjang; i++) {
             System.out.print("*  ");
         }
-        for (int i =2; i<lebar;i++){
+        for (int i = 2; i < lebar; i++) {
             System.out.println();
             System.out.print("*  ");
-            for (int j =2;j<panjang;j++){
+            for (int j = 2; j < panjang; j++) {
                 System.out.print("   ");
             }
             System.out.print("*  ");
         }
         System.out.println();
-        for (int i = 0;i<panjang;i++){
+        for (int i = 0; i < panjang; i++) {
             System.out.print("*  ");
+        }
+    }
+
+    public static void jajarGenjang(int panjang, int tinggi) {
+        int barisMaju = tinggi - 1;
+//        for (int i =0;i<tinggi;i++){
+//            for (int j=0;j<panjang;j++){
+//                System.out.print("*  ");
+//            }
+//            System.out.println();
+
+
+        for (int k = 0; k < barisMaju; k++) {
+            System.out.print("   ");
+        }
+        barisMaju--;
+        for (int i = 0; i < panjang; i++) {
+            System.out.print("*  ");
+        }
+        for (int i = 2; i < tinggi; i++) {
+            for (int k = 0; k < barisMaju; k++) {
+                System.out.print("   ");
+            }
+            barisMaju--;
+            System.out.println();
+            System.out.print("*  ");
+            for (int j = 2; j < panjang; j++) {
+                System.out.print("   ");
+            }
+            System.out.print("*  ");
+        }
+        System.out.println();
+        for (int i = 0; i < panjang; i++) {
+            System.out.print("*  ");
+        }
+    }
+
+    public static void jajar() {
+        int x = 8, y = 5;
+        int spasi = y - 1;
+        for (int i = 0; i < y; i++) {
+            for (int k = 0; k < spasi; k++) {
+                System.out.print("   ");
+            }
+            spasi--;
+            for (int j = 0; j < x; j++) {
+                System.out.print("*  ");
+            }
+            System.out.println();
         }
     }
 }
+
