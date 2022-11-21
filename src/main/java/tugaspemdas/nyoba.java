c60059440a732df879db2316ec6e657bda92a375
Xkylz1 <fauzansaputra741@gmail.com>
2022-11-21 20:07:37 +0800

    nambahin method persegi panjang

--- src/main/java/tugaspemdas/nyoba.java
+++ src/main/java/tugaspemdas/nyoba.java
@@ -6,10 +6,13 @@ import java.util.Scanner;
 public class nyoba {
     public static void main(String[] args) {
 Scanner input = new Scanner(System.in);
-        System.out.println("Masukan panjang sisi: ");
-//        int panjangSisi = input.nextInt();
-        int panjangSisi=5;
-        persegi(panjangSisi);
+        System.out.print("Masukan panjang : ");
+        int panjangSisi = input.nextInt();
+        System.out.print("Masukan lebar : ");
+        int lebar = input.nextInt();
+        persegiPanjang(panjangSisi,lebar);
+//        int panjangSisi=5;
+//        persegi(panjangSisi);
 
     }
 
@@ -27,6 +30,24 @@ Scanner input = new Scanner(System.in);
         }
         System.out.println();
         for(int i =0;i<sisi;i++) {
+            System.out.print("*  ");
+        }
+    }
+
+    public static void persegiPanjang(int panjang, int lebar){
+        for (int i = 0;i<panjang;i++){
+            System.out.print("*  ");
+        }
+        for (int i =2; i<lebar;i++){
+            System.out.println();
+            System.out.print("*  ");
+            for (int j =2;j<panjang;j++){
+                System.out.print("   ");
+            }
+            System.out.print("*  ");
+        }
+        System.out.println();
+        for (int i = 0;i<panjang;i++){
             System.out.print("*  ");
         }
     }
